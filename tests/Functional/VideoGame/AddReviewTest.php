6a4f587d75ef331f549f3e78f750f9c552efb86c
Anthony Delbee <anthony.delbee@gmail.com>
2025-09-30 15:55:54 +0200

    ajout tests fonctionnel pour vérifier: - User non logé => pas d'accès - Mauvaise data dans form => exception - controle données dans db

--- tests/Functional/VideoGame/AddReviewTest.php
+++ tests/Functional/VideoGame/AddReviewTest.php
@@ -8,13 +8,14 @@ use App\Model\Entity\Review;
 use App\Model\Entity\User;
 use App\Model\Entity\VideoGame;
 use App\Tests\Functional\FunctionalTestCase;
+use http\Exception\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Response;
 
 final class AddReviewTest extends FunctionalTestCase
 {
 
     /**
-     * Test si on affiche le formulaire de review lorsque user logué
+     * Test si on affiche le formulaire de review lorsque user logué et pas de review
      *
      * @return void
      */
@@ -26,6 +27,17 @@ final class AddReviewTest extends FunctionalTestCase
         $this->get('/jeu-video-0');
         self::assertSelectorExists('form');
         self::assertSelectorTextContains('form button, form input[type="submit"]', 'Poster');
+    }
+
+    /**
+     * Test l'absence du formulaire si user non logué
+     *
+     * @return void
+     */
+    public function testShouldNotShowReviewForm(): void
+    {
+        $crawler = $this->get('/jeu-video-0');
+        self::assertTrue($crawler->filter('form')->count() == 0);
     }
 
     public function testShouldRedirectAfterSubmitForm(): void
@@ -52,14 +64,32 @@ final class AddReviewTest extends FunctionalTestCase
             ])
         ;
 
+        //Vérification de la sauvegarde de la review dans DB
         self::assertSame(5, $savedRating->getRating()); // on contrôle dans la base que la valeur est bien 5
+        self::assertSame('Test', $savedRating->getComment()); // on contrôle le commentaire
+
+        // vérification de la redirection après soumission du formulaire
         self::assertResponseRedirects('/jeu-video-0');
         $crawler = $this->client->followRedirect(); // on suit la redirection
 
-        // on vérifie l'abscence du formulaire après la redirection (1 seule review / user / jeu)
+        // on vérifie l'absence du formulaire après la redirection (1 seule review / user / jeu)
         self::assertTrue($crawler->filter('form')->count() == 0);
+    }
 
-        $this->ensureReviewNotPublishedByUser('user+0@email.com'); //@todo plus necessaire avec dama
+    public function testShouldShowErrorOnSubmitFormWithWrongData(): void
+    {
+        $this->ensureReviewNotPublishedByUser('user+0@email.com');
+
+        $this->login('user+0@email.com');
+        $crawler =$this->get('/jeu-video-0');
+        $form = $crawler->selectButton('Poster')->form();
+
+        self::expectExceptionMessage('Input "review[rating]" cannot take "" as a value (possible values: "1", "2", "3", "4", "5").');
+        $this->client->submit($form , [
+            'review[rating]' => '',
+            'review[comment]' => ''
+        ]);
+        self::assertResponseIsUnprocessable(); // Lève l'exception avant d'arriver ici
     }
 
     // Contrôle si une review n'a pas été généré par les fixtures
