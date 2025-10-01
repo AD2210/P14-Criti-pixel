399f52cce361396206edf7f7ba8f0efd793b2913
Anthony Delbee <anthony.delbee@gmail.com>
2025-10-01 11:13:52 +0200

    Configuration de l'env de test pour recharger une base propre à chaque test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,17 @@ if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
 
+// create up-to-date TEST database before all tests start
+passthru('php bin/console doctrine:database:drop --env=test --force --if-exists');
+passthru('php bin/console doctrine:database:create --env=test');
+passthru('php bin/console doctrine:migrations:migrate --env=test -n');
+passthru('php bin/console doctrine:fixtures:load --env=test -n');
+
+// remove TEST database after all tests end
+register_shutdown_function(function() {
+    passthru('php bin/console doctrine:database:drop --env=test --force --if-exists');
+});
+
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
