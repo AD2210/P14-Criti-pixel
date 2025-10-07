ac39e7094495fe00bd1911b36d122fdefd3283ba
Anthony Delbee <anthony.delbee@gmail.com>
2025-10-07 21:54:39 +0200

    Correction d'un erreur dans typage d'une variable d'un test, ajout suppression sécurisé en éliminant les connexions active à la DB

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,6 +14,7 @@ passthru('php bin/console doctrine:fixtures:load --env=test -n');
 
 // remove TEST database after all tests end
 register_shutdown_function(function () {
+    passthru('php -r "require \'vendor/autoload.php\'; (new Symfony\Component\Dotenv\Dotenv())->bootEnv(\'' . addslashes(dirname(__DIR__) . '/.env') . '\'); $url=parse_url(getenv(\'DATABASE_URL\')); $db=trim($url[\'path\'], \'/\'); $host=$url[\'host\']??\'localhost\'; $port=$url[\'port\']??5432; $user=$url[\'user\']??\'postgres\'; $pass=$url[\'pass\']??\'\'; $cmd=sprintf(\'PGPASSWORD=%s psql -h %s -p %d -U %s -d postgres -v ON_ERROR_STOP=1 -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = \'\'%s\'\' AND pid <> pg_backend_pid();"\' , escapeshellarg($pass), escapeshellarg($host), $port, escapeshellarg($user), $db); passthru($cmd);";');
     passthru('php bin/console doctrine:database:drop --env=test --force --if-exists');
 });
 
